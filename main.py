478e0d576a674b797c5de453429822a20c5788cc
hzxwonder <52537483+hzxwonder@users.noreply.github.com>
2023-01-08 16:03:32 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import requests
+
+import csv
+from tqdm import tqdm
+from lxml import etree
+from lxml.etree import HTMLParser
+import argparse
+import os
+
+
+def getHTMLText(url):
+    kv = {'user_agent':'Mozilla/5.0'}
+    proxies_myself = {'http':'105.27.238.167:80'}
+    try:
+        r = requests.get(url,headers = kv,proxies = proxies_myself,timeout=120)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        print('Failed! Please check the conference name ,conference year and the keyword!')
+        return ''
+
+def writeToCsv(args,dicts):
+    if not args.save_dir:
+        args.save_dir = '.'
+    else:
+        if not os.path.exists(args.save_dir):
+            os.mkdir(args.save_dir)
+
+    if args.keyword:
+        file_path = os.path.join(args.save_dir,"{}_{}_{}.csv".format(args.name,args.time,args.keyword))
+    else:
+        file_path = os.path.join(args.save_dir,"{}_{}.csv".format(args.name,args.time))
+
+    with open(file_path,'w',encoding='utf-8',newline='') as f:
+        csv_write = csv.writer(f)
+        csv_head = ["Title","Authors"]
+        csv_write.writerow(csv_head)
+        for ele in dicts:
+            csv_write.writerow([ele['title'],"\n".join(ele['authors'])])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Conference Information")
+    parser.add_argument('-n',"--name", type=str, required=True,help="Name of Conference you want to search.")
+    parser.add_argument('-t',"--time", type=int, default=2022, help="Year of Conference you want to search.")
+    parser.add_argument("--save_dir", type=str, default=None, help="the file directory which you want to save to.")
+    parser.add_argument('-k',"--keyword", type=str, default=None, help="the keyword filter, if None, save all the paper found.")
+    args = parser.parse_args()
+
+    print("Looking for papers from conferences {} {}, keyword: {}".format(args.name,args.time,args.keyword))
+    url = "https://dblp.org/db/conf/{}/{}{}.html".format(args.name,args.name,args.time)
+    print("Parsing URL: {}".format(url))
+    htmltext = getHTMLText(url)
+    try:
+        parse_html = etree.HTML(htmltext,HTMLParser())
+        parse_xpaths = parse_html.xpath('//li[@class="entry inproceedings"]')
+    except:
+        print('Failed! Please check the conference name ,conference year and the keyword!')
+        exit(1)
+
+    dics = []
+    print("Number of papers(all fields): {}".format(len(parse_xpaths)))
+    for parse_xpath in tqdm(parse_xpaths):
+        parse_html_str = etree.tostring(parse_xpath)
+        parse_html1 = etree.HTML(parse_html_str,HTMLParser())
+        parse_content = parse_html1.xpath('//cite//span[@itemprop="name"]')
+        parse_content = [parse_content[idx].text for idx in range(len(parse_content))]
+        if args.keyword:
+            paper_title = parse_content[-1].upper()
+            keyword = args.keyword.upper()
+            if paper_title.find(keyword) == -1:
+                # print(parse_content[-1])
+                continue
+            else:
+                dic = {"title": parse_content[-1], "authors": parse_content[:-1]}
+                dics.append(dic)
+        else:
+            dic = {"title": parse_content[-1], "authors": parse_content[:-1]}
+            dics.append(dic)
+
+    writeToCsv(args,dics)
+    print("The number of Papers extracted: {}".format(len(dics)))
+
+
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Computer Science Conference Paper Extractor Script
+
+This is a python script to extract conference paper from dblp : https://dblp.org/db/conf/index.html
+
+## Requirements
+
+```text
+lxml==4.9.2
+requests==2.25.1
+tqdm==4.62.3
+```
+
+## Steps
+
+### First Step : Install Dependency
+
+```shell
+pip install -r requirements.txt
+```
+
+### Second Step : Run main.py
+
+```python
+python main.py --name cvpr --time 2019 --keyword reinforce
+```
+
+* "-n" or "--name" Name of Conference you want to search, it is required. You must check whether the name is right or it will throw a exception after 2 minutes.
+* "-t" or --time" Year of Conference you want to search. If not specified, it will fill the year of "2022".
+* "-k" or "--keyword" means the keyword you want to filter the results founded. If not specified, it will output all the paper accepted. You don't need to pay attention to the case of keywords, because the code already has uniform case.
+* "--save_dir" means the file directory which you want to save to.
+
+And if all the processes are correct, it will output a .csv file containing the titles and authors of all (keyword) papers.
+
+If you have any questions, you can submit your issue. I will tackle it as soon as possible!
